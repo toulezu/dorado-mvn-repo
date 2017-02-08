c1ca63f88572d241cfd3ce59092a2abb993d76bd
ck <svsechen@163.com>
2017-02-08 10:28:06 +0800

    初始提交

---
+++ src/main/java/com/ckjava/dorado/App.java
@@ -0,0 +1,13 @@
+package com.ckjava.dorado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
